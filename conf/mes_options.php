d55178d4fe1bfbee9398cde15dd8145863385166
magikcypress <cyp@rouquin.me>
2017-02-23 02:07:28 +0100

    rewrite dans la conf de nginx

--- conf/mes_options.php
+++ conf/mes_options.php
@@ -1,5 +1,5 @@
 <?php
 # Pour simuler une redirection Apache
 define('_SERVER_APACHE', 'false');
-define('_DIR_CONNECT', 'config');
-define('_DIR_TMP', 'tmp');
+// define('_DIR_CONNECT', 'config');
+// define('_DIR_TMP', 'tmp');
